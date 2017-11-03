bc10084739baa2b4fb8a1c803f8ace100c5376a7
markm <markm@vm0.p2-mr-prefetch.netopt-pg0.wisc.cloudlab.us>
2017-11-03 16:17:47 -0500

    Don't build jar if build failed

--- mr_test/build.sh
+++ mr_test/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export HADOOP_CP=/home/ubuntu/software/hadoop-2.6.0:/home/ubuntu/conf:/home/ubuntu/software/hive-1.2.1:/home/ubuntu/software/tez-0.7.1-SNAPSHOT-minimal/*:/home/ubuntu/software/tez-0.7.1-SNAPSHOT-minimal/lib/*::/home/ubuntu/conf:/home/ubuntu/software/hadoop-2.6.0/share/hadoop/common/lib/*:/home/ubuntu/software/hadoop-2.6.0/share/hadoop/common/*:/home/ubuntu/software/hadoop-2.6.0/share/hadoop/hdfs:/home/ubuntu/software/hadoop-2.6.0/share/hadoop/hdfs/lib/*:/home/ubuntu/software/hadoop-2.6.0/share/hadoop/hdfs/*:/home/ubuntu/software/hadoop-2.6.0/share/hadoop/yarn/lib/*:/home/ubuntu/software/hadoop-2.6.0/share/hadoop/yarn/*:/home/ubuntu/software/hadoop-2.6.0/share/hadoop/mapreduce/lib/*:/home/ubuntu/software/hadoop-2.6.0/share/hadoop/mapreduce/*
+export HADOOP_CP=/users/markm/software/hadoop-2.6.0:/users/markm/conf:/users/markm/software/hive-1.2.1:/users/markm/software/tez-0.7.1-SNAPSHOT-minimal/*:/users/markm/software/tez-0.7.1-SNAPSHOT-minimal/lib/*::/users/markm/conf:/users/markm/software/hadoop-2.6.0/share/hadoop/common/lib/*:/users/markm/software/hadoop-2.6.0/share/hadoop/common/*:/users/markm/software/hadoop-2.6.0/share/hadoop/hdfs:/users/markm/software/hadoop-2.6.0/share/hadoop/hdfs/lib/*:/users/markm/software/hadoop-2.6.0/share/hadoop/hdfs/*:/users/markm/software/hadoop-2.6.0/share/hadoop/yarn/lib/*:/users/markm/software/hadoop-2.6.0/share/hadoop/yarn/*:/users/markm/software/hadoop-2.6.0/share/hadoop/mapreduce/lib/*:/users/markm/software/hadoop-2.6.0/share/hadoop/mapreduce/*
 
-javac -cp ${HADOOP_CP} -d classes/ MRApplication.java
-jar -cvf ac.jar -C classes/ .
+javac -cp ${HADOOP_CP} -d classes/ MRApplication.java && jar -cvf ac.jar -C classes/ .
