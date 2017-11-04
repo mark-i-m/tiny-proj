6a057ac9d85ecbaeba8bc1bd57d11de89334c91e
markm <markm@vm0.p2-mr-prefetch.netopt-pg0.wisc.cloudlab.us>
2017-11-03 22:20:30 -0500

    modified mr program; added log files

--- mr_test/MRApplication.java
+++ mr_test/MRApplication.java
@@ -15,10 +15,10 @@ public class MRApplication {
     // How much to amplify the input. For each value of input, there is this
     // many values output by the mapper. So given a 1M line input, AMP = 2^10
     // yields about a 8GB (2^10 * 1M * 8B) because a long is 8B.
-    private static final long AMP = 1 << 10;
+    private static final long AMP = 1 << 0;
     
     public static class TestMapper
-        extends Mapper<LongWritable, Text, LongWritable, LongWritable>
+        extends Mapper<LongWritable, Text, IntWritable, IntWritable>
     {
         private Random rng = new Random();
         private static final int N = Integer.MAX_VALUE;
@@ -26,26 +26,30 @@ public class MRApplication {
         protected void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException
         {
+            Configuration conf = context.getConfiguration();
+            int numReducers = Integer.parseInt(conf.get("numReducers"));
             for (long i = 0; i < AMP; i++) {
-                long val = (long)rng.nextInt(N);
-                context.write(new LongWritable(val), new LongWritable(val));
+                int val = rng.nextInt(N) % numReducers;
+                context.write(new IntWritable(val), new IntWritable(val));
             }
         }
     }
 
     public static class TestReducer
-        extends Reducer<LongWritable, Text, NullWritable, NullWritable>
+        extends Reducer<IntWritable, IntWritable, NullWritable, NullWritable>
     {
-        protected void reduce(LongWritable key, Iterable<Text> values, Context context)
+        protected void reduce(IntWritable key, Iterable<IntWritable> values, Context context)
             throws IOException, InterruptedException
         { 
             // only output the values
-            //context.write(NullWritable.get(), NullWritable.get());
+            for (IntWritable v : values)
+                context.write(NullWritable.get(), NullWritable.get());
         }
     }
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
+        conf.set("numReducers", args[2]);
         Job job = Job.getInstance(conf, "test");
 
         job.setJarByClass(MRApplication.class);
@@ -55,8 +59,8 @@ public class MRApplication {
 
         job.setNumReduceTasks(Integer.parseInt(args[2]));
 
-        job.setMapOutputKeyClass(LongWritable.class);
-        job.setMapOutputValueClass(LongWritable.class);
+        job.setMapOutputKeyClass(IntWritable.class);
+        job.setMapOutputValueClass(IntWritable.class);
 
         job.setOutputKeyClass(NullWritable.class);
         job.setOutputValueClass(NullWritable.class);
