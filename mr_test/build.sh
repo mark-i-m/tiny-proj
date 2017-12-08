6716f9a5c4332a1849b4c2ad0b3d25ef6dab0f39
Mark Mansi <markm@cs.wisc.edu>
2017-12-07 19:38:14 -0600

    Minor fix

--- mr_test/build.sh
+++ mr_test/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p classes
+
 export HADOOP_CP=/users/markm/software/hadoop-2.6.0:/users/markm/conf:/users/markm/software/hive-1.2.1:/users/markm/software/tez-0.7.1-SNAPSHOT-minimal/*:/users/markm/software/tez-0.7.1-SNAPSHOT-minimal/lib/*::/users/markm/conf:/users/markm/software/hadoop-2.6.0/share/hadoop/common/lib/*:/users/markm/software/hadoop-2.6.0/share/hadoop/common/*:/users/markm/software/hadoop-2.6.0/share/hadoop/hdfs:/users/markm/software/hadoop-2.6.0/share/hadoop/hdfs/lib/*:/users/markm/software/hadoop-2.6.0/share/hadoop/hdfs/*:/users/markm/software/hadoop-2.6.0/share/hadoop/yarn/lib/*:/users/markm/software/hadoop-2.6.0/share/hadoop/yarn/*:/users/markm/software/hadoop-2.6.0/share/hadoop/mapreduce/lib/*:/users/markm/software/hadoop-2.6.0/share/hadoop/mapreduce/*
 
 javac -cp ${HADOOP_CP} -d classes/ MRApplication.java && jar -cvf ac.jar -C classes/ .
